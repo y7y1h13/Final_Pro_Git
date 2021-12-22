41274badfd70ea9f5f951bd88f45ca852252bce8
HNM123 <2049905@naver.com>
2021-12-22 15:58:41 +0900

    Initial commit

---
+++ DAO.py
@@ -0,0 +1,44 @@
+class JjalDao:
+    def connect(self):
+        return pymysql.connect(host='127.0.0.1', user='bigdata',
+                              password='dbgusals1',db='chat',charset='utf8')
+
+    def sel_action(self,word):
+        conn = self.connect()
+        sql = "select idx from action where 단어=%s"
+        cursor = conn.cursor()
+        cursor.execute(sql,word)
+        result = cursor.fetchall()
+        conn.close()
+        return result
+        
+    def sel_emotion(self,word):
+        conn = self.connect()
+        sql = "select idx from emotion where 단어=%s"
+        cursor = conn.cursor()
+        cursor.execute(sql,word)
+        result = cursor.fetchall()
+        conn.close()
+        return result
+        
+    def sel_object(self,word):
+        conn = self.connect()
+        sql = "select idx from object where 단어=%s"
+        cursor = conn.cursor()
+        cursor.execute(sql,word)
+        result = cursor.fetchall()
+        conn.close()
+        return result
+        
+    def sel_main(self,idx):
+        conn = self.connect()
+        sql = "select URL from main where IDX=%s"
+        cursor = conn.cursor()
+        cursor.execute(sql,idx)
+        result = cursor.fetchall()
+        conn.close()
+        return result
+
+
+    
+    
